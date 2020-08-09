c12128554b407f0fb9a0d1b3aa4c656e78608f5d
Nathaniel-insight <nagillgren@gmail.com>
2020-08-08 23:10:25 -0700

    updated load_data

---
+++ load_data.py
@@ -0,0 +1,23 @@
+"""
+Purpose -
+This is where you create your loading scripts for your data
+main support for this file will be
+pandas read_csv
+sqlalchemy
+google cloud platform
+"""
+
+def load():
+    """
+    main function for loading data
+    :return:
+    """
+
+    return None
+
+if __name__ == '__main__':
+    """
+    perform load_data tests here 
+    """
+
+    data = load()
