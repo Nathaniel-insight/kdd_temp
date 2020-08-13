1e7c336ee19d2a26b6cb0a237f4a7a28b7c1483f
Nathaniel-insight <nagillgren@gmail.com>
2020-08-12 19:58:01 -0700

    updated clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -7,10 +7,11 @@ sklearn
 """
 
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
 from load_data import load
 
 
-class clean_data(BaseEstimator, TransformerMixin):
+class CleanData(BaseEstimator, TransformerMixin):
     # Class Constructor
     def __init__(self):
         return None
@@ -29,11 +30,22 @@ class clean_data(BaseEstimator, TransformerMixin):
 
         return X
 
-def clean_pipe():
+
+def c_pipe():
     """
     add standard sklearn transformed here as a part of a pip
     :return:
     """
+
+    steps = [
+        ('custom_clean', CleanData())
+        # add named steps and other standard transformers here
+        # ('standard scaler', StandardScaler())
+    ]
+
+    clean_pipe = Pipeline(steps=steps)
+
+    return clean_pipe
 
 
 if __name__ == "__main__":
@@ -42,3 +54,7 @@ if __name__ == "__main__":
     """
 
     data = load()
+
+    clean_pipe = c_pipe()
+    clean_data = clean_pipe(data)
+
